05721b7442a09c01b6810a9b4a78df1b6355109c
ssisaias <ssisaias@users.noreply.github.com>
2023-08-17 23:42:56 -0300

    adds global style and default layout

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,23 @@
 export const defaultTheme = {
   white: '#FFF',
 
-  'gray-100': '#E1E1E6',
-  'gray-300': '#C4C4CC',
-  'gray-400': '#8D8D99',
-  'gray-500': '#7C7C8A',
-  'gray-600': '#323238',
-  'gray-700': '#29292E',
-  'gray-800': '#202024',
-  'gray-900': '#121214',
+  'background': 'FAFAFA',
 
   'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
+  'base-card': '#F3F2F2',
+  'base-input': '#EDEDED',
+  'base-button': '#E6E5E5',
+  'base-hover': '#D7D5D5',
+  'base-label': '#8D8686',
+  'base-text': '#574F4D',
+  'base-subtitle': '#403937',
+  'base-title': '#272221',
 
-  'red-500': '#AB222E',
-  'red-700': '#7A1921',
+  'purple-dark': '#4B2995',
+  'purple': '#8047F8',
+  'purple-light': '#EBE5F9',
 
-  'yellow-500': '#FBA94C',
+  'yellow-dark': '#DBAC2C',
+  'yellow': '#C47F17',
+  'yellow-light': '#F1E9C9',
 }
