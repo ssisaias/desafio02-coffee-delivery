5f6d64efc3a0782e9085d1ac7841897f74f6a67e
ssisaias <ssisaias@users.noreply.github.com>
2023-08-19 19:52:06 -0300

    Implements Homepage banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,18 +1,41 @@
-import { BannerContainer } from "./styles";
+import { BannerContainer, RoundCircle } from "./styles";
 import BannerCup  from '../../assets/banner_cup.svg'
+import { IconContext, ShoppingCart, Timer, Package, Coffee } from "@phosphor-icons/react";
 
 export function Banner(){
     return (
         <BannerContainer>
+            <IconContext.Provider
+                value={{
+                    color: 'white',
+                    size: '1rem',
+                    weight: "fill",
+                }}
+            >
             <div>
                 <h1>Encontre o café perfeito <br /> para qualquer hora do dia</h1>
                 <span>Com o Cooffee Delivery você recebe seu café onde estiver, a qualquer hora</span>
-
-                <p>Compra Simples e Segura</p>
-                <p>Entrega rápida e rastreada</p>
-                <p>Embalagem mantém o café intacto</p>
-                <p>O café chega fresquinho até você</p>
+                
+                <table>
+                    <tr>
+                        <td><RoundCircle $themeBgColor="yellow-dark"><ShoppingCart /></RoundCircle></td>
+                        <td>Compra Simples e Segura</td>
+                    </tr>
+                    <tr>
+                        <td><RoundCircle $themeBgColor="yellow"><Timer /></RoundCircle></td>
+                        <td>Entrega rápida e rastreada</td>
+                    </tr>
+                    <tr>
+                        <td><RoundCircle><Package /></RoundCircle></td>
+                        <td>Embalagem mantém o café intacto</td>
+                    </tr>
+                    <tr>
+                        <td><RoundCircle $themeBgColor="purple"><Coffee /></RoundCircle></td>
+                        <td>O café chega fresquinho até você</td>
+                    </tr>
+                </table>
             </div>
+            </IconContext.Provider>
             <div>
                 <img src={BannerCup} alt="Banner Cup" />
             </div>
--- src/components/Banner/styles.tsx
+++ src/components/Banner/styles.tsx
@@ -1,18 +1,20 @@
 import { styled } from "styled-components";
 
 export const BannerContainer = styled.div`
-    border: 1px solid #000;
-    background-color: ${props => props.theme['purple-light']};
-    display: none;
     flex-direction: column;
     flex-shrink: 0;
     gap: 1rem;
+    text-align: left;
 
     @media (min-width: 768px) {
         display: flex;
         flex-direction: row;
     }
 
+    @media (max-width: 768px) {
+        /* hide the image in smallers screens */
+        img{display: none}
+    }
     img{
         max-width: 80%;
         height: auto;
@@ -42,4 +44,23 @@ export const BannerContainer = styled.div`
         font-size: 16px;
         color: ${props => props.theme['base-subtitle']};
     }
+
+    table {
+        display: table-cell;
+        column-count: 2;
+        padding: 1rem;
+    }
+`
+export const RoundCircle = styled.div<{ $themeBgColor?: string; }>`
+    width: 2rem;
+    height: 2rem;
+    background-color: ${props => (props.$themeBgColor ? props.theme[props.$themeBgColor] : props.theme['base-text'])};
+    border-radius: 66.6rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    margin-right: 8px;
 `
