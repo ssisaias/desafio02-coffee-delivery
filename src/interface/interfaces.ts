0938cb307c548707ec2d34e8dcd693db8fc5c5a7
ssisaias <ssisaias@users.noreply.github.com>
2023-08-23 15:21:21 -0300

    Coffee image resolver

---
+++ src/interface/interfaces.ts
@@ -0,0 +1,8 @@
+export interface CoffeeInterface {
+    id: string
+    name: string
+    description: string | null
+    price: number
+    tags: string[] | null
+    image: string
+}
---
+++ src/util/imageResolver.ts
@@ -0,0 +1,37 @@
+import Americano from '../assets/americano.png';
+import Cafecomleite from '../assets/cafecomleite.png';
+import Chocolatequente from '../assets/chocolatequente.png';
+import Expressocremoso from '../assets/expressocremoso.png';
+import Latte from '../assets/latte.png';
+import Mochaccino from '../assets/mochaccino.png';
+import Arabe from '../assets/arabe.png';
+import Cafegelado from '../assets/cafegelado.png';
+import Cubano from '../assets/cubano.png';
+import Havaiano from '../assets/havaiano.png';
+import Capuccino from '../assets/capuccino.png';
+import Expresso from '../assets/expresso.png';
+import Irlandes from '../assets/irlandes.png';
+import Macchiato from '../assets/macchiato.png';
+
+interface Images {
+    [key: string]: string;
+}
+
+const images: Images = {
+    Americano,
+    Cafecomleite,
+    Chocolatequente,
+    Expressocremoso,
+    Latte,
+    Mochaccino,
+    Arabe,
+    Cafegelado,
+    Cubano,
+    Havaiano,
+    Capuccino,
+    Expresso,
+    Irlandes,
+    Macchiato,
+}
+
+export default images;
