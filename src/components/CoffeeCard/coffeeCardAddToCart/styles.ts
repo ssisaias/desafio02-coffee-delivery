72f1c229fa5ae57d2d2e78c8ed0a3f5d76571eb8
ssisaias <ssisaias@users.noreply.github.com>
2023-08-25 15:20:39 -0300

    finish add to card styles

--- src/components/CoffeeCard/coffeeCardAddToCart/styles.ts
+++ src/components/CoffeeCard/coffeeCardAddToCart/styles.ts
@@ -2,15 +2,41 @@ import { styled } from "styled-components";
 
 export const CCAddToCartContainer = styled.div`
     display: flex;
+    flex-direction: row;
     width: 13rem;
-    justify-content: space-between;
+    height: auto;
     align-items: center; 
+    margin-left: 1.5rem;
 `
 
-export const CCAddToCartPriceTag = styled.button`
-
+export const CCAddToCartPriceTag = styled.div`
+    font-family: 'Baloo 2';
+    font-size: 1.5rem;
+    font-weight: 500;
+    color: ${props => props.theme['base-text']};
 `
 
-export const CCAddToCartQuantity = styled.button`
+export const CCAddToCartQuantity = styled.div`
+    max-width: 4.5rem;
+    height: 1rem;
+    padding: 0.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.25rem;
+    border-radius: 0.375rem;
+    background-color: ${props => props.theme['base-button']};
+    margin-left: 0.5rem;
 
+    button{
+        border: none;
+        cursor: pointer;
+        padding-top: 4px;
+        border-radius: 0.3rem;
+        background: none;
+    }
+`
+
+export const CCAddToCartButton = styled.div`
+    margin: 0 0 0.5rem 0.8rem;
 `
--- src/components/CoffeeCard/index.tsx
+++ src/components/CoffeeCard/index.tsx
@@ -1,40 +1,51 @@
-import { ShoppingCart } from "@phosphor-icons/react"
+import { Minus, MinusSquare, Plus, PlusSquare, ShoppingCart } from "@phosphor-icons/react"
 import { CoffeeInterface } from "../../interface/interfaces"
 import images from "../../util/imageResolver"
 import { SquareIconBg } from "../ui/IconBackground/styles"
-import { CCAddToCartContainer } from "./coffeeCardAddToCart/styles"
+import { CCAddToCartButton, CCAddToCartContainer, CCAddToCartPriceTag, CCAddToCartQuantity } from "./coffeeCardAddToCart/styles"
 import { CCHeaderImg } from "./coffeeCardImgHeader/styles"
 import { CCHeaderTag, CCHeaderTags } from "./coffeeCardTags/styles"
 import { CoffeeCardContainer, CCNameDescriptionContainer } from "./styles"
 
 interface CoffeeCardProps {
-    coffeeItem: CoffeeInterface
+  coffeeItem: CoffeeInterface
 }
 
 
-export function CoffeeCard({coffeeItem}: CoffeeCardProps){
-    
-    return (
-        <CoffeeCardContainer>
-            <CCHeaderImg>
-                <img src={images[coffeeItem.image]} alt={coffeeItem.image} />
-            </CCHeaderImg>
-            <CCHeaderTags>
-                {coffeeItem.tags?.map((tag) => {
-                    return <CCHeaderTag>{tag}</CCHeaderTag>
-                })}
-            </CCHeaderTags>
-            <CCNameDescriptionContainer>
-                <h4>{coffeeItem.name}</h4>
-                <p>{coffeeItem.description}</p>
-            </CCNameDescriptionContainer>
-            <CCAddToCartContainer>
-                    
-                    <SquareIconBg onClick={() => {console.log("add to cart")}} $themeBgColor="purple-dark" $cursor="pointer">
-                        <ShoppingCart></ShoppingCart>
-                    </SquareIconBg>
-                
-            </CCAddToCartContainer>
-        </CoffeeCardContainer>
-    )
+export function CoffeeCard({ coffeeItem }: CoffeeCardProps) {
+
+  return (
+    <CoffeeCardContainer>
+      <CCHeaderImg>
+        <img src={images[coffeeItem.image]} alt={coffeeItem.image} />
+      </CCHeaderImg>
+      <CCHeaderTags>
+        {coffeeItem.tags?.map((tag) => {
+          return <CCHeaderTag>{tag}</CCHeaderTag>
+        })}
+      </CCHeaderTags>
+      <CCNameDescriptionContainer>
+        <h4>{coffeeItem.name}</h4>
+        <p>{coffeeItem.description}</p>
+      </CCNameDescriptionContainer>
+      <CCAddToCartContainer>
+
+        <CCAddToCartPriceTag>
+          {coffeeItem.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+        </CCAddToCartPriceTag>
+
+        <CCAddToCartQuantity>
+          <button><Minus weight="regular" size={'0.875rem'} color="#8047F8"></Minus></button>
+            <span>1</span>
+          <button><Plus weight="regular" size={'0.875rem'} color="#8047F8"></Plus></button>
+        </CCAddToCartQuantity>
+        <CCAddToCartButton>
+          <SquareIconBg title="Adicionar ao carrinho" onClick={() => { console.log("add to cart") }} $themeBgColor="purple-dark" $cursor="pointer">
+            <ShoppingCart ></ShoppingCart>
+          </SquareIconBg>
+        </CCAddToCartButton>
+
+      </CCAddToCartContainer>
+    </CoffeeCardContainer>
+  )
 }
