471f79702e53003ec4e4d8774838f6b9cc9fd071
ssisaias <ssisaias@users.noreply.github.com>
2023-08-27 10:39:38 -0300

    improves dark theme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -28,24 +28,24 @@ export const defaultTheme: DefaultTheme = {
 export const darkTheme: DefaultTheme = {
   white: '#FFF',
 
-  'background': '#000',
+  'background': '#101523',
 
   'green-300': '#00B37E',
-  'base-card': '#F3F2F2',
-  'base-input': '#EDEDED',
-  'base-button': '#E6E5E5',
-  'base-hover': '#D7D5D5',
-  'base-label': '#8D8686',
-  'base-text': '#574F4D',
-  'base-subtitle': '#403937',
-  'base-title': '#272221',
+  'base-card': '#0f0a1e',
+  'base-input': '#1f1346',
+  'base-button': '#1f1346',
+  'base-hover': '#1f1346',
+  'base-label': '#575757',
+  'base-text': '#575757',
+  'base-subtitle': '#7a7a7a',
+  'base-title': '#7a7a7a',
   'toggle-button': '#4B2995',
 
   'purple-dark': '#4B2995',
   'purple': '#8047F8',
   'purple-light': '#EBE5F9',
 
-  'yellow-dark': '#DBAC2C',
-  'yellow': '#C47F17',
-  'yellow-light': '#F1E9C9',
+  'yellow-dark': '#4C4000',
+  'yellow': '#DBAC2C',
+  'yellow-light': '#FFD700',
 }
