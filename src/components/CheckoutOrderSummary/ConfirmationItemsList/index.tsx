d2b1ec1a3608b6a4c4499d0bffc099875519e68e
ssisaias <ssisaias@users.noreply.github.com>
2023-09-24 17:29:00 -0300

    Change cart quantity from checkout summary

--- src/components/CheckoutOrderSummary/ConfirmationItemsList/index.tsx
+++ src/components/CheckoutOrderSummary/ConfirmationItemsList/index.tsx
@@ -3,25 +3,48 @@ import { CoffeeCardProps } from "../../../props/commonsProps";
 import images from "../../../util/imageResolver";
 
 import { CSMiddleContainer, CheckoutSummaryItemCard, CheckoutSummaryQuantityButtons, CheckoutSummaryRemoveButton } from "./styles";
+import { CartContext } from "../../../contexts/CartContext";
+import { useContext } from "react";
 
-export function ConfirmationItemList({ coffeeItem }: CoffeeCardProps){
-    return (
-        <CheckoutSummaryItemCard>
-            <img src={images[coffeeItem.image]} alt="cart-item" />
-            <div>
-                <div>Expresso tradicional</div>
-                <CSMiddleContainer>
-                    <CheckoutSummaryQuantityButtons>
-                    <button><Minus weight="regular" size={'0.875rem'} color="#8047F8"></Minus></button>
-                        <span>99</span>
-                    <button><Plus weight="regular" size={'0.875rem'} color="#8047F8"></Plus></button>    
-                    </CheckoutSummaryQuantityButtons>   
-                    <CheckoutSummaryRemoveButton>
-                        <Trash weight="regular" color="#8047F8"></Trash> <span>Remover</span>
-                    </CheckoutSummaryRemoveButton>                 
-                </CSMiddleContainer>
-            </div>
-            <p>R$ 9999,9999</p>
-        </CheckoutSummaryItemCard>
-    )
+
+export function ConfirmationItemList({ coffeeItem, quantity }: CoffeeCardProps) {
+
+  const { setItemQuantity } = useContext(CartContext);
+
+  function handleQuantityIncrease() {
+    if (quantity && quantity < 99) {
+      setItemQuantity(coffeeItem.id, (quantity + 1));
+    }
+  }
+
+  function handleQuantityDecrease() {
+    if (quantity && quantity > 1) {
+      setItemQuantity(coffeeItem.id, (quantity - 1));
+    }
+  }
+
+  function singleItemPrice() {
+    const price = coffeeItem.price * (quantity ? quantity : 0);
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(price);
+  }
+
+  return (
+    <CheckoutSummaryItemCard>
+      <img src={images[coffeeItem.image]} alt="cart-item" />
+      <div>
+        <div>{coffeeItem.name}</div>
+        <CSMiddleContainer>
+          <CheckoutSummaryQuantityButtons>
+            <button onClick={handleQuantityDecrease}><Minus weight="regular" size={'0.875rem'} color="#8047F8"></Minus></button>
+            <span>{quantity}</span>
+            <button onClick={handleQuantityIncrease}><Plus weight="regular" size={'0.875rem'} color="#8047F8"></Plus></button>
+          </CheckoutSummaryQuantityButtons>
+          <CheckoutSummaryRemoveButton>
+            <Trash weight="regular" color="#8047F8"></Trash> <span>Remover</span>
+          </CheckoutSummaryRemoveButton>
+        </CSMiddleContainer>
+      </div>
+      <p>{singleItemPrice()}</p>
+    </CheckoutSummaryItemCard>
+  )
 }
